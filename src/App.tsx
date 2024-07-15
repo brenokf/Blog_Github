951c7358fc52943e3ccf099c14ebbfcae1e08ccc
Breno Melo Mendes <breno.mm@kyndryl.com>
2024-07-15 13:22:48 -0400

    add Provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
+import { GithubBlogProvider } from './context/GithubBlogContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
+        <GithubBlogProvider>
+          <Router />
+        </GithubBlogProvider>
       </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
